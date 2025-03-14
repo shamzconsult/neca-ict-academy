1583bcb235e80016ecd967ef0bcfc6182e6ffd11
Shamz Consult <166094785+shamzconsult@users.noreply.github.com>
2025-03-14 08:44:41 +0100

    Revert "partners logo updated" (#27)

--- src/components/atom/Partners.tsx
+++ src/components/atom/Partners.tsx
@@ -8,27 +8,25 @@ import { SubHeading } from "./headers/SubHeading";
 export const Partners = () => {
   return (
     <div className="max-w-5xl mx-auto px-4 my-20">
-      <div className="flex flex-col justify-center items-center text-center gap-3 mb-4">
+      <div className="flex flex-col justify-center items-center text-center gap-3 mb-6">
         <SubHeading>Partners</SubHeading>
         <Heading>Organisations That Trusts Us</Heading>
       </div>
-      <div className="max-w-xl mx-auto">
-        <Marquee speed={50} pauseOnHover={false}>
-          {partners?.map((partner, index) => (
-            <div
-              key={index}
-              className="w-fit h-48  flex justify-center items-center mx-16 "
-            >
-              <Image
-                src={partner?.src}
-                alt={partner?.alt}
-                width={100}
-                height={100}
-              />
-            </div>
-          ))}
-        </Marquee>
-      </div>
+      <Marquee speed={50} pauseOnHover={false}>
+        {partners?.map((partner, index) => (
+          <div
+            key={index}
+            className="w-fit h-32  flex justify-center items-center mx-6 "
+          >
+            <Image
+              src={partner?.src}
+              alt={partner?.alt}
+              width={100}
+              height={100}
+            />
+          </div>
+        ))}
+      </Marquee>
     </div>
   );
 };
