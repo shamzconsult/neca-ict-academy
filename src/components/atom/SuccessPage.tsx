7c560559c081daa6b6b738d95097bac126abc8f9
Abbascodde <abbasyim1@gmail.com>
2025-03-22 10:14:19 +0000

    updated

---
+++ src/components/atom/SuccessPage.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image';
+import { SubHeading } from "@/components/atom/headers/SubHeading";
+
+const SuccessPage = () => {
+
+
+
+  return (
+    <div>
+      <div className="absolute top-0 left-0 w-1/5 h-2/2 z-0">
+        <Image
+          src="https://res.cloudinary.com/daqmbfctv/image/upload/e_improve,e_sharpen/v1742225179/Rectangle_4384_onnutg.png"
+          alt="Background Left"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-40"
+        />
+      </div>
+      <div className="absolute top-0 right-0 w-3/7 h-3/1 z-0">
+        <Image
+          src="https://res.cloudinary.com/daqmbfctv/image/upload/v1742225179/Rectangle_4383_akoej5.png"
+          alt="Background Right"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-20"
+        />
+      </div>
+      <div className="absolute top-0 right-0 w-1/7 mt-180 h-full z-0">
+      </div>
+      <div className="relative max-w-6xl mx-auto my-20 mt-[150px] p-6 bg-white rounded-lg overflow-hidden">
+        <div className="relative flex flex-col w-[540px] items-center justify-center min-h-screen mx-auto">
+          <Image
+            src="https://res.cloudinary.com/daqmbfctv/image/upload/e_improve,e_sharpen/v1742551374/tnddp2bamc3iu2pznihy.png"
+            alt="Success Icon"
+            width={100}
+            height={100}
+            className='mb-8'
+          />
+            
+          <SubHeading>Thanks For Registering!</SubHeading>
+
+          <h2 className="text-[#27156F] text-2xl font-semibold mt-6">
+            Check Your Email
+          </h2>
+          <p className='mt-4 text-center text-xl'>You will receive a confirmation email with details about the next steps.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessPage;
