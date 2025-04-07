2187bbadb523e6b1824887467e8417c28420f0cd
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2025-04-07 09:07:04 +0100

    Chore: ID get api setup

--- src/app/api/cohort/[_id]/route.ts
+++ src/app/api/cohort/[_id]/route.ts
@@ -2,6 +2,41 @@ import connectViaMongoose from "@/lib/db"
 import Cohort from "@/models/cohort";
 import { NextResponse } from "next/server";
 
+
+const GET = async (req: Request) => {
+    try {
+        await connectViaMongoose()
+        const url = new URL(req.url);
+        const id = url.pathname.split("/").pop();
+
+        if (!id) {
+            return NextResponse.json(
+                { message: "Applicant ID is required" },
+                { status: 400 }
+            );
+        }
+
+        const cohort = await Cohort.findById(id);
+        if (!cohort) {
+            return NextResponse.json(
+                { message: "Cohort not found" },
+                { status: 404 }
+            )
+        }
+
+        return NextResponse.json(
+            { cohort },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error fetching cohort:", error);
+        return NextResponse.json(
+            { message: "Error fetching cohort", error },
+            { status: 500 }
+        );
+    }
+}
+
 const PUT = async (req: Request) => {
     try{
         await connectViaMongoose();
@@ -58,4 +93,4 @@ const DELETE = async (req: Request) => {
         );
     }
 }
-export { PUT, DELETE }
+export { GET, PUT, DELETE }
