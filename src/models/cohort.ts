62b2eb786d0d1bf15462c8adcb7d895fec04284d
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2025-04-25 11:57:04 +0100

    Chore: mongodb setup

--- src/models/cohort.ts
+++ src/models/cohort.ts
@@ -54,7 +54,7 @@ const CohortSchema = new Schema(
   }
 );
 
-// Updated pre-save hook
+// Updated pre-save 
 CohortSchema.pre('save', async function (next) {
   if (!this.isModified('name') && this.slug) return next();
 
