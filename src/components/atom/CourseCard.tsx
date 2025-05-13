c79d3061e009049dd22002e6036fc6b5fefd939b
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2025-05-13 10:11:14 +0100

    Chore: update course card badge

--- src/components/atom/CourseCard.tsx
+++ src/components/atom/CourseCard.tsx
@@ -273,11 +273,11 @@ export const CourseCard = ({
                 {/* {(isCoursesPath || admin) && ( */}
                 <div className='flex items-center gap-2 text-xs'>
                   {course.hasCertificate && (
-                    <div className='px-1 bg-green-100 text-green-700 rounded font-semibold'>
+                    <div className='px-1 bg-gray-100 text-gray-700 rounded font-semibold'>
                       Certificate
                     </div>
                   )}
-                  <div className='px-1 bg-blue-100 text-blue-700 rounded font-semibold'>
+                  <div className='px-1 bg-gray-100 text-gray-700 rounded font-semibold'>
                     {course.type}
                   </div>
                 </div>
