4abfb34bbd752659e6513b09d6dabcbe67b36557
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2026-06-14 23:21:29 +0100

    feat: course per row to 3

--- src/components/molecules/courses/CoursesCards.tsx
+++ src/components/molecules/courses/CoursesCards.tsx
@@ -9,19 +9,21 @@ export const CoursesCards = ({ courses }: { courses: CourseType[] }) => {
 
   return (
     <div>
-      <section className="mb-20 flex flex-col justify-center items-center">
-        <div className="bg-[#DBEAF6] w-full py-16">
-          <div className="max-w-6xl mx-auto flex flex-col justify-center items-center gap-3 text-center mb-4">
+      <section className='mb-20 flex flex-col justify-center items-center'>
+        <div className='bg-[#DBEAF6] w-full py-16'>
+          <div className='max-w-6xl mx-auto flex flex-col justify-center items-center gap-3 text-center mb-4'>
             <SubHeading>Explore Courses</SubHeading>
             <Heading>Build the Skills for a Future in Tech!</Heading>
-            <p className="max-w-4xl md:text-[20px]">
+            <p className='max-w-4xl md:text-[20px]'>
               Discover industry-relevant courses designed to equip you with the
               knowledge and hands-on experience needed to excel in today&#8217;s
               digital world.
             </p>
           </div>
         </div>
-        <CourseCard courses={filteredData} />
+        <div className='max-w-7xl'>
+          <CourseCard courses={filteredData} columnsPerRow={3} />
+        </div>
       </section>
     </div>
   );
